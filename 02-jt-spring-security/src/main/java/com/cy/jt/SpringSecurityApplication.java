53edc1d83242c5e6d6158f0f7c412893c9dc21af
Miyazaki <woshisurui132>
2021-07-20 08:55:46 +0800

    Add application.yml

--- 02-jt-spring-security/src/main/java/com/cy/jt/SpringSecurityApplication.java
+++ 02-jt-spring-security/src/main/java/com/cy/jt/SpringSecurityApplication.java
@@ -2,11 +2,19 @@ package com.cy.jt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityApplication.class,args);
+        encodePwd();
     }
-
+    //通过Spring Security 中的BCryptPasswordEncoder基于明文创建一个密文
+    static void encodePwd(){
+        BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
+        String password="123456";//明文
+        String newPwd=encoder.encode("123456");
+        System.out.println(newPwd);
+    }
 }
