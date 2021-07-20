daa888fa77e312159ef471131d66f60b2e2c59b2
your-name <your-email@youremail.com>
2021-07-20 20:09:51 +0800

    Add RedirectAuthenticationSuccessHandler.java

--- 02-jt-spring-security/src/main/java/com/cy/jt/security/config/SecurityConfig.java
+++ 02-jt-spring-security/src/main/java/com/cy/jt/security/config/SecurityConfig.java
@@ -1,14 +1,40 @@
 package com.cy.jt.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
 
 /**
  * 由@Configuration注解描述的类为spring中的配置类,配置类会在spring
  * 工程启动时优先加载,在配置类中通常会对第三方资源进行初始配置.
  */
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        //super.configure(http)
+        //1.关闭跨域攻击
+        http.csrf().disable();
+        //2.配置登录url(登陆表单使用哪个页面)
+        http.formLogin().loginPage("/login.html")//登录页面
+                        .loginProcessingUrl("/login")//与form表单中的action值相同
+                        //.usernameParameter("username")//与form表单中input元素的name属性相同
+                        //.passwordParameter("password")
+                        //.successForwardUrl("/index.html");//请求转发
+                        .defaultSuccessUrl("/index.html");//登陆成功的url
+                        //.successHandler(new RedirectAuthenticationSuccessHandler("http://www.tedu.cn"));
+                        //.failureHandler()
+                        //.successForwardUrl("/index.html");//get
+                        //.failureUrl("/login.html?error");//登陆失败(默认)
+        //3.放行登录url(不需要验证就可以访问)
+        http.authorizeRequests()
+                .antMatchers("/login.html","/images/**")//这里写你要放行的资源
+                .permitAll()//允许直接访问
+                .anyRequest().authenticated();//除了以上资源必须认证才可访问
+    }
     /**
      * 定义SpringSecurity密码加密对象
      * @Bean 注解通常会在@Configuration注解描述的类中描述方法,
---
+++ 02-jt-spring-security/src/main/java/com/cy/jt/security/controller/PageController.java
@@ -0,0 +1,18 @@
+package com.cy.jt.security.controller;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class PageController {
+    @RequestMapping("/index")
+    public String toIndex(HttpServletRequest request){
+        String username=request.getParameter("username");
+        System.out.println("toIndex()=>"+username);
+        //spring mvc 中的重定向
+        return "redirect:/index.html";
+    }
+}
